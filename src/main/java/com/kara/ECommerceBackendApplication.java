4d8d7cfbcca4dabc273ed796629c12376f8f5227
Furkan KARA - Junior Java Developer <58552366+Furkan9528@users.noreply.github.com>
2021-06-02 16:12:50 +0200

    Backend-Spring Boot

--- src/main/java/com/kara/ECommerceBackendApplication.java
+++ src/main/java/com/kara/ECommerceBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ECommerceBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ECommerceBackendApplication.class, args);
 	}
 
